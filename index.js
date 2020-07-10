e1e825468cda9cec90bb9a2081b7cb1a022c3272
Johnny Rice <jrice@labact.com>
2020-07-10 09:55:02 -0400

    Lower loop max from 2000 to 200 or lower

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
 /* 
  @Challenge 03 - Write a function named `showMeTheMoney` that loops from 1 to 200. 
  Use a condition in the loop to console.log the number when it is over 190
- @Example - Expect to see 19991, 192, 193, 194, 195, 196, 197, 198, 199, 200 in the terminal
+ @Example - Expect to see 191, 192, 193, 194, 195, 196, 197, 198, 199, 200 in the terminal
  @Test - Make a function call of  `showMeTheMoney()`
 */
 
