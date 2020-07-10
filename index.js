db9af5a32ee050ee399821dd133bc60cb8203370
Johnny Rice <jrice@labact.com>
2020-07-10 09:57:49 -0400

    Remove 'to' type

--- index.js
+++ index.js
@@ -36,7 +36,7 @@
 /* 
  @Challenge 05 - Write a function that returns an array that is named `getTheTwenties` that loops from 1 to 60 and returns an array of all the 20's
  Use a condition in the loop to push to an array when the number is greater than or equal to 20 and under 30
- @Example - Expect to an array returned with value of [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+ @Example - Expect an array returned with value of [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
  @Test - console.log the result of a function call of  `getTheTwenties()` and expect to see an array value of [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 */
 
